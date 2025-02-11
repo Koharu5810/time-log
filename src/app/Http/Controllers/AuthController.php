e07086fa6af92c36c4c09171fde0a820dc3c7b6a
koharu <le2.lsb108@gmail.com>
2025-02-11 14:53:58 +0900

    コントローラ作成

---
+++ src/app/Http/Controllers/AuthController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\UserRegisterRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Auth\Events\Registered;
+
+class AuthController extends Controller
+{
+// 会員登録画面表示
+    public function showRegistrationForm() {
+        return view('user.register');
+    }
+// 会員登録処理
+    public function register(UserRegisterRequest $request) {
+    // public function register(Request $request) {
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        event(new Registered($user));
+        // 登録直後にログイン状態にする
+        auth()->login($user);
+
+        return redirect()->route('attendance.create');
+    }
+
+// ログイン画面表示
+    public function showUsersLoginForm() {
+        return view('user.login');
+    }
+// ログイン処理
+    // public function login(LoginRequest $request) {
+    //     $user = User::where('name', $request->name)
+    //         ->orWhere('email', $request->username)  // ユーザ名またはメールアドレスで認証
+    //         ->first();
+
+    //     // パスワードが一致するか確認
+    //     if ($user && Hash::check($request->password, $user->password)) {
+    //         auth()->login($user);  // 認証成功
+
+    //         // 認証成功後商品一覧画面にリダイレクト
+    //         return redirect()->route('home');
+    //     }
+
+    //     session()->flash('auth_error', 'ログイン情報が登録されていません');
+    //     return redirect()->route('login')->withInput();  // 入力値を保持してログイン画面にリダイレクト
+    // }
+}
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,18 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\AttendanceCreateController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+// 会員登録画面
+Route::get('/user/register', [AuthController::class, 'showRegistrationForm'])->withoutMiddleware(['auth'])->name('user.register.show');
+Route::post('/user/register', [AuthController::class, 'register'])->withoutMiddleware(['auth'])->name('user.register');
+// 一般ユーザログイン画面
+Route::get('/user/login', [AuthController::class, 'showusersLoginForm'])->name('user.login.show');
+Route::post('/user/login', [AuthController::class, 'login'])->name('user.login');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/', [AttendanceCreateController::class, 'index'])->name('create');
+});
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('user.login');
 });
