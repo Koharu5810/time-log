cd03e2d78332d192e25cc28a80da559afae228a1
koharu <le2.lsb108@gmail.com>
2025-02-14 23:41:32 +0900

    web.php HTTPパス修正

--- src/routes/web.php
+++ src/routes/web.php
@@ -5,20 +5,21 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AttendanceController;
 
 // 会員登録画面
-Route::get('/user/register', [AuthController::class, 'showRegistrationForm'])->withoutMiddleware(['auth'])->name('user.register.show');
-Route::post('/user/register', [AuthController::class, 'register'])->withoutMiddleware(['auth'])->name('user.register');
+Route::get('/register', [AuthController::class, 'showRegistrationForm'])->withoutMiddleware(['auth'])->name('user.register.show');
+Route::post('/register', [AuthController::class, 'register'])->withoutMiddleware(['auth'])->name('user.register');
 // 一般ユーザログイン画面
-Route::get('/user/login', [AuthController::class, 'showUsersLoginForm'])->name('user.login.show');
-Route::post('/user/login', [AuthController::class, 'login'])->name('user.login');
+Route::get('/login', [AuthController::class, 'showUsersLoginForm'])->name('user.login.show');
+Route::post('/login', [AuthController::class, 'login'])->name('user.login');
 
 // ログアウト機能
-Route::post('/user/logout', [AuthController::class, 'userDestroy'])->name('user.logout');
+Route::post('/logout', [AuthController::class, 'userDestroy'])->name('user.logout');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/', [AttendanceController::class, 'index'])->name('create');
-    Route::post('/user', [AttendanceController::class, 'store'])->name('attendance.store');
+    Route::get('/attendance', [AttendanceController::class, 'index'])->name('create');
+    Route::post('/attendance', [AttendanceController::class, 'store'])->name('attendance.store');
 
-    Route::get('/user/attendance-list', [AttendanceController::class, 'showUserAttendanceList'])->name('user.attendance.list');
-    Route::get('/user/request-list', [AttendanceController::class, 'showRequestList'])->name('request.list');
-    Route::get('/user/attendance-detail', [AttendanceController::class, 'showAttendanceDetail'])->name('request.detail');
+    Route::get('/attendance/list', [AttendanceController::class, 'showUserAttendanceList'])->name('user.attendance.list');
+    // ↓一般・管理者同様パス。認証ミドルウェアで区別を実装
+    Route::get('/stamp_correction_request/list', [AttendanceController::class, 'showRequestList'])->name('request.list');
+    Route::get('/attendance/{id}', [AttendanceController::class, 'showAttendanceDetail'])->name('request.detail');
 });
