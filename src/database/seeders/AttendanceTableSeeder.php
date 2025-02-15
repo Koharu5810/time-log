744a0d5eead82a653585a75c03c5d8a1472e1942
koharu <le2.lsb108@gmail.com>
2025-02-15 13:18:16 +0900

    シーダファイル修正

--- src/database/seeders/AttendanceTableSeeder.php
+++ src/database/seeders/AttendanceTableSeeder.php
@@ -2,9 +2,7 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\User;
 use App\Models\Attendance;
 use App\Models\BreakTime;
 use Carbon\Carbon;
