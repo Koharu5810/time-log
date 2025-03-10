7c67525ed4ee3736fecb54ba458aa6ab9640a953
koharu <le2.lsb108@gmail.com>
2025-03-10 22:42:41 +0900

    AttendanceListTest／勤怠一覧画面に遷移し現在の月が表示されるテスト作成

--- src/tests/Feature/AttendanceListTest.php
+++ src/tests/Feature/AttendanceListTest.php
@@ -7,11 +7,13 @@ use Illuminate\Foundation\Testing\WithFaker;
 use App\Models\Attendance;
 use Tests\TestCase;
 use Tests\Helpers\TestHelper;
+use Carbon\Carbon;
 
 class AttendanceListTest extends TestCase
 {
     use RefreshDatabase;
 
+// 勤怠情報がすべて表示されるテスト
     public function test_user_can_see_all_attendance_records(): void
     {
         $this->seed();
@@ -33,5 +35,21 @@ class AttendanceListTest extends TestCase
             $work_time = $attendance->duration_in_minutes - $attendance->total_break_time;
             $response->assertSee($work_time);
         }
+    }
+//勤怠一覧画面に遷移した際に現在の月が表示されるテスト
+    public function test_attendance_list_displays_current_month(): void
+    {
+        $this->seed();
+
+        $user = TestHelper::userLogin();
+        /** @var \App\Models\User $user */  // 型解析ツールのエラー防止
+        $this->actingAs($user);
+
+        $response = $this->get(route('attendance.list'));
+        $response->assertStatus(200);
+
+        // 現在の月を取得し、ページ内に表示されていることを確認
+        $currentMonth = Carbon::now()->format('Y/m');
+        $response->assertSee($currentMonth);
     }
 }
