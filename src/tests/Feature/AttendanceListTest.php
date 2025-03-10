0bc3c1be9478c3d9524a216c6cbd3f857055c01b
koharu <le2.lsb108@gmail.com>
2025-03-10 23:31:16 +0900

    AttendanceListTest／勤怠一覧画面から勤怠詳細画面へ遷移するテスト作成

--- src/tests/Feature/AttendanceListTest.php
+++ src/tests/Feature/AttendanceListTest.php
@@ -3,8 +3,8 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use App\Models\Attendance;
+use App\Models\BreakTime;
 use Tests\TestCase;
 use Tests\Helpers\TestHelper;
 use Carbon\Carbon;
@@ -34,9 +34,10 @@ class AttendanceListTest extends TestCase
             // 勤務合計時間 (勤務時間 - 休憩時間) を計算し、表示されていることを確認
             $work_time = $attendance->duration_in_minutes - $attendance->total_break_time;
             $response->assertSee($work_time);
+            $response->assertSee('詳細');
         }
     }
-//勤怠一覧画面に遷移した際に現在の月が表示されるテスト
+// 勤怠一覧画面に遷移した際に現在の月が表示されるテスト
     public function test_attendance_list_displays_current_month(): void
     {
         $this->seed();
@@ -93,5 +94,27 @@ class AttendanceListTest extends TestCase
         $response = $this->get(route('attendance.list', ['year' => Carbon::now()->addMonth()->year, 'month' => Carbon::now()->addMonth()->month]));
         $response->assertStatus(200);
         $response->assertSee($nextMonthDisplay);
+    }
+// 詳細を押すと詳細画面に遷移する
+    public function test_can_access_attendance_detail_page(): void
+    {
+        $user = TestHelper::userLogin();
+        /** @var \App\Models\User $user */  // 型解析ツールのエラー防止
+        $this->actingAs($user);
+
+        $attendance = Attendance::create([
+            'user_id'   => $user->id,
+            'work_date' => Carbon::now()->toDateString(),
+            'clock_in'  => Carbon::now()->setHour(9)->setMinute(0)->toTimeString(),
+            'clock_end' => Carbon::now()->setHour(18)->setMinute(0)->toTimeString(),
+        ]);
+
+        $response = $this->get(route('attendance.list'));
+        $response->assertStatus(200);
+        $response->assertSeeText('詳細');
+        $response->assertSee(route('attendance.detail', ['id' => $attendance->id]));
+
+        $response = $this->get(route('attendance.detail', ['id' => $attendance->id]));
+        $response->assertStatus(200);
     }
 }
