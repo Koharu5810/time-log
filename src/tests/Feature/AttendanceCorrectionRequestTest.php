a519f3c5b6f7fce0a0e408b4cfd2c6c70cfb3ae5
koharu <le2.lsb108@gmail.com>
2025-03-12 16:40:26 +0900

    AttendanceCorrectionRequest／出勤時間他退勤時間よりあとの場合のバリデーションテスト作成

--- src/tests/Feature/AttendanceCorrectionRequestTest.php
+++ src/tests/Feature/AttendanceCorrectionRequestTest.php
@@ -3,7 +3,8 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;use App\Models\Attendance;
+use App\Models\Attendance;
+use App\Models\AttendanceCorrectRequest;
 use App\Models\BreakTime;
 use Tests\TestCase;
 use Tests\Helpers\TestHelper;
@@ -13,6 +14,9 @@ use Carbon\Carbon;
 class AttendanceCorrectionRequestTest extends TestCase
 {
     use RefreshDatabase;
+
+    private $user;
+    private $attendance;
 
     private function createAttendanceStatus($user)
     {
@@ -26,14 +30,28 @@ class AttendanceCorrectionRequestTest extends TestCase
         $breakTime = BreakTime::create([
             'attendance_id' => $attendance->id,
             'break_time_start' => '12:00:00',
-            'break_time_end' => '12:00:00',
+            'break_time_end' => '13:00:00',
         ]);
 
         $attendance->breakTimes()->save($breakTime);
+
+        return $attendance;
+    }
+    private function openLoginPage($attendance)
+    {
+        return $this->get(route('attendance.detail', ['id' => $attendance->id]))
+            ->assertStatus(200)
+            ->assertSee('<form', false);
+    }
+    private function assertValidationError($attendance, $data, $expectedErrors)
+    {
+        $response = $this->put(route('attendance.update', ['id' => $attendance->id]), $data);
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors($expectedErrors);
     }
 
-// 名前欄がログインユーザの氏名の表示
-    public function test_attendance_detail_displays_logged_in_user_name(): void
+// 出勤時間が退勤時間より後になっている場合バリデーションメッセージ表示
+    public function test_validation_error_when_clock_in_is_after_clock_end()
     {
         $user = TestHelper::userLogin();
         /** @var \App\Models\User $user */   // $userの型解析ツールエラーが出るため追記
@@ -41,31 +59,50 @@ class AttendanceCorrectionRequestTest extends TestCase
 
         $attendance = $this->createAttendanceStatus($user);
 
-        // 勤怠データがログインユーザーと紐づいているか確認
-        $this->assertEquals($user->id, $attendance->user_id);
-
-        $response = $this->get(route('attendance.detail', ['id' => $attendance->id]));
-        $response->assertStatus(200);
-
-        $response->assertSeeText($user->name);
-    }
-
-    public function test_email_validation_error_when_email_is_missing()
-    {
-        $this->openLoginPage();
-
-        Admin::create([
-            'name' => 'admin1',
-            'email' => 'admin@example.com',
-            'password' => bcrypt('password123'),
-        ]);
+        $this->openLoginPage($attendance);
 
         $data = [
-            'email' => '',
-            'password' => 'password123',
+            'attendance_id' => $attendance->id,
+            'requested_clock_in' => '18:30',
+            'requested_clock_end' => '17:30',
+            'remarks' => '早出のため',
+            'request_status' => '承認待ち',
         ];
-        $expectedErrors = ['email' => 'メールアドレスを入力してください'];
+        $expectedErrors = ['requested_clock_end' => '出勤時間もしくは退勤時間が不適切な値です'];
 
-        $this->assertValidationError($data, $expectedErrors);
+        $this->assertValidationError($attendance,$data, $expectedErrors);
+
+        // 修正リクエストがデータベースに保存されていないことを確認
+        $this->assertDatabaseMissing('attendance_correct_requests', [
+            'attendance_id' => $attendance->id,
+            'requested_clock_in' => Carbon::parse('18:30')->format('H:i:s'),
+            'requested_clock_end' => Carbon::parse('17:30')->format('H:i:s'),
+        ]);
+
+        // $response = $this->post(route('attendance.update'), $data);
+        // $response->assertStatus(302);
+
+        // $this->assertDatabaseHas('attendance_correct_requests', [
+        //     'user_id' => ,
+        //     'attendance_id' => ,
+        //     'previous_clock_in' => ,
+        //     'previous_clock_end' => ,
+        //     'attendance_id' => $this->attendance->id,
+        //     'requested_clock_in' => '18:30:00',
+        //     'requested_clock_end' => '17:30:00',
+        //     'remarks' => '早出のため',
+        //     'request_status' => '承認待ち',
+        //     'admin_id' => null,
+        //     'approved_at' => null,
+        // ]);
+
+        // BreakTimeCorrectRequest::create([
+        //     'break_time_id' => ,
+        //     'att_correct_id' => ,
+        //     'previous_break_time_start' => ,
+        //     'previous_break_time_end' => ,
+        //     'requested_break_time_start' => ,
+        //     'requested_break_time_end' => ,
+        // ]);
     }
 }
