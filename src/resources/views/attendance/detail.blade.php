d870606a3eb66e2cc2a10aacebeda65ee5146297
koharu <le2.lsb108@gmail.com>
2025-03-12 22:06:15 +0900

    attendance.detailエラーメッセージ配置修正

--- src/resources/views/attendance/detail.blade.php
+++ src/resources/views/attendance/detail.blade.php
@@ -18,7 +18,7 @@
         {{-- 名前 --}}
                 <tr>
                     <td class="table__label">名前</td>
-                    <td class="table__input">
+                    <td class="table__input name-display">
                         <input
                             type="hidden"
                             name="user_id"
@@ -30,7 +30,7 @@
         {{-- 日付 --}}
                 <tr>
                     <td class="table__label">日付</td>
-                    <td class="table__input">
+                    <td class="table__input work_date-display">
                         <input
                             type="hidden"
                             name="work_date"
@@ -44,33 +44,38 @@
                 <tr>
                     <td class="table__label">出勤・退勤</td>
                     <td class="table__input">
-                        @if ($attendance->attendanceCorrectRequest)
-                            {{ \Carbon\Carbon::parse($attendance->attendanceCorrectRequest->requested_clock_in)->format('H:i') }}
-                        @else
-                            <input
-                                type="text"
-                                name="requested_clock_in"
-                                value="{{ old('requested_clock_in', $attendance->clock_in ? \Carbon\Carbon::parse($attendance->clock_in)->format('H:i') : '') }}"
-                            />
-                        @endif
+                        <div class="attendance-time-group">
+                            <div class="attendance-time-inputs">
+                                @if ($attendance->attendanceCorrectRequest)
+                                    {{ \Carbon\Carbon::parse($attendance->attendanceCorrectRequest->requested_clock_in)->format('H:i') }}
+                                @else
+                                    <input
+                                        type="text"
+                                        name="requested_clock_in"
+                                        value="{{ old('requested_clock_in', $attendance->clock_in ? \Carbon\Carbon::parse($attendance->clock_in)->format('H:i') : '') }}"
+                                    />
+                                @endif
 
-                        <span>〜</span>
+                                <span>〜</span>
 
-                        @if ($attendance->attendanceCorrectRequest)
-                            {{ \Carbon\Carbon::parse($attendance->attendanceCorrectRequest->requested_clock_end)->format('H:i') }}
-                        @else
-                            <input
-                                type="text"
-                                name="requested_clock_end"
-                                value="{{ old('requested_clock_end', $attendance->clock_end ? \Carbon\Carbon::parse($attendance->clock_end)->format('H:i') : '') }}"
-                            />
-                        @endif
-                        @if ($errors->has('requested_clock_in') || $errors->has('requested_clock_end'))
-                            <div class="error-message">
-                                <div>@error('requested_clock_in') {{ $message }} @enderror</div>
-                                <div>@error('requested_clock_end') {{ $message }} @enderror</div>
+                                @if ($attendance->attendanceCorrectRequest)
+                                    {{ \Carbon\Carbon::parse($attendance->attendanceCorrectRequest->requested_clock_end)->format('H:i') }}
+                                @else
+                                    <input
+                                        type="text"
+                                        name="requested_clock_end"
+                                        value="{{ old('requested_clock_end', $attendance->clock_end ? \Carbon\Carbon::parse($attendance->clock_end)->format('H:i') : '') }}"
+                                    />
+                                @endif
                             </div>
-                        @endif
+
+                            @if ($errors->has('requested_clock_in') || $errors->has('requested_clock_end'))
+                                <div class="error-message">
+                                    <div>@error('requested_clock_in') {{ $message }} @enderror</div>
+                                    <div>@error('requested_clock_end') {{ $message }} @enderror</div>
+                                </div>
+                            @endif
+                        </div>
                     </td>
                 </tr>
         {{-- 休憩 --}}
@@ -80,50 +85,57 @@
                     <tr>
                         <td class="table__label">{{ $index == 0 ? '休憩' : '休憩' . ($index + 1) }}</td>
                         <td class="table__input">
-                            @if ($break['is_corrected'])
-                                {{ \Carbon\Carbon::parse($break['start'])->format('H:i') }}
-                            @else
-                                <input
-                                    type="text"
-                                    name="break_times[{{ $index }}][start]"
-                                    value="{{ old("break_times.{$break['index']}.start", $break['start'] ? \Carbon\Carbon::parse($break['start'])->format('H:i') : '') }}"
-                                />
-                            @endif
+                            <div class="break-time-group">
+                                <div class="break-time-inputs">
+                                    @if ($break['is_corrected'])
+                                        {{ \Carbon\Carbon::parse($break['start'])->format('H:i') }}
+                                    @else
+                                        <input
+                                            type="text"
+                                            name="break_times[{{ $index }}][start]"
+                                            value="{{ old("break_times.{$break['index']}.start", $break['start'] ? \Carbon\Carbon::parse($break['start'])->format('H:i') : '') }}"
+                                        />
+                                    @endif
 
-                            <span>〜</span>
+                                    <span>〜</span>
 
-                            @if ($break['is_corrected'])
-                                {{ \Carbon\Carbon::parse($break['end'])->format('H:i') }}
-                            @else
-                                <input
-                                    type="text"
-                                    name="break_times[{{ $index }}][end]"
-                                    value="{{ old("break_times.{$break['index']}.end", $break['end'] ? \Carbon\Carbon::parse($break['end'])->format('H:i') : '') }}"
-                                />
-                            @endif
-                            @if ($errors->has("break_times.{$index}.start") || $errors->has("break_times.{$index}.end"))
-                                <div class="error-message">
-                                    <div>@error("break_times.{$index}.start") {{ $message }} @enderror</div>
-                                    <div>@error("break_times.{$index}.end"){{ $message }} @enderror</div>
+                                    @if ($break['is_corrected'])
+                                        {{ \Carbon\Carbon::parse($break['end'])->format('H:i') }}
+                                    @else
+                                        <input
+                                            type="text"
+                                            name="break_times[{{ $index }}][end]"
+                                            value="{{ old("break_times.{$break['index']}.end", $break['end'] ? \Carbon\Carbon::parse($break['end'])->format('H:i') : '') }}"
+                                        />
+                                    @endif
                                 </div>
-                            @endif
+
+                                @if ($errors->has("break_times.{$index}.start") || $errors->has("break_times.{$index}.end"))
+                                    <div class="error-message">
+                                        <div>@error("break_times.{$index}.start") {{ $message }} @enderror</div>
+                                        <div>@error("break_times.{$index}.end"){{ $message }} @enderror</div>
+                                    </div>
+                                @endif
+                            </div>
                         </td>
                     </tr>
                 @endforeach
         {{-- 備考 --}}
                 <tr>
                     <td class="table__label">備考</td>
-                    <td class="table__input">
+                    <td class="table__input remarks_textarea">
                         @if ($attendance->attendanceCorrectRequest)
                             {{ $attendance->attendanceCorrectRequest->remarks }}
                         @else
-                            <textarea name="remarks" placeholder="電車遅延のため">{{ old('remarks') }}</textarea>
+                            <div style="display: flex; flex-direction: column;">
+                                <textarea name="remarks" placeholder="電車遅延のため">{{ old('remarks') }}</textarea>
+                                @if ($errors->has('remarks'))
+                                    <div class="error-message" style="display: block; margin-top: 5px;">
+                                        @error('remarks') {{ $message }} @enderror
+                                    </div>
+                                @endif
+                            </div>
                         @endif
-                            @if ($errors->has('remarks'))
-                                <div class="error-message">
-                                    @error('remarks') {{ $message }} @enderror
-                                </div>
-                            @endif
                     </td>
                 </tr>
             </tbody>
