52c3cfd14523015f46bc5f4afd296c528d4386f7
koharu <le2.lsb108@gmail.com>
2025-03-12 14:23:11 +0900

    AttendanceCorrectionRequestTest／共通処理作成

--- src/tests/Feature/AttendanceCorrectionRequestTest.php
+++ src/tests/Feature/AttendanceCorrectionRequestTest.php
@@ -3,18 +3,33 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\WithFaker;use App\Models\Attendance;
+use App\Models\BreakTime;
 use Tests\TestCase;
+use Tests\Helpers\TestHelper;
+use Carbon\Carbon;
+
 
 class AttendanceCorrectionRequestTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    private function createAttendanceStatus($user)
+    {
+        $attendance = Attendance::create([
+            'user_id' => $user->id,
+            'work_date' => now()->format('Y-m-d'),
+            'clock_in' => '09:00:00',
+            'clock_end' => '18:00:00',
+        ]);
+
+        $breakTime = BreakTime::create([
+            'attendance_id' => $attendance->id,
+            'break_time_start' => '12:00:00',
+            'break_time_end' => '12:00:00',
+        ]);
+
+        $attendance->breakTimes()->save($breakTime);
     }
+
 }
